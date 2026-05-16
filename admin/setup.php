e76acb672830ff5766dd482b82e8a2f006236b05
gc-cobia <gc@cobia.fr>
2026-05-16 23:28:21 +0200

    Update setup.php

--- admin/setup.php
+++ admin/setup.php
@@ -1,93 +1,97 @@
 <?php
 
-// Load Dolibarr environment
-$res = 0;
-if (!$res && file_exists("../../../main.inc.php")) $res = @include "../../../main.inc.php";
-if (!$res && file_exists("../../../../main.inc.php")) $res = @include "../../../../main.inc.php";
+// Chargement de l'environnement Dolibarr
+require '../../main.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 
-if (!$res) die("Include of main.inc.php failed");
-
-require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
-require_once DOL_DOCUMENT_ROOT . '/core/class/html.form.class.php';
-
-// Load language files
+// Chargement des fichiers de langue
 $langs->loadLangs(array("admin", "qontosync@qontosync"));
 
-// Security check
+// Vérification des droits d'accès
 if (!$user->admin) {
-    accessforbidden();
+	accessforbidden();
 }
 
-$action = GETPOST('action', 'alpha');
+$action = GETPOST('action', 'aZ09');
+$backtopage = GETPOST('backtopage', 'alpha');
+
+// Liste des constantes à gérer
+$constantes = array(
+	'QONTOSYNC_API_KEY',
+	'QONTOSYNC_SLUG'
+);
 
 /*
  * Actions
  */
 
 if ($action == 'update') {
-    $error = 0;
+	$error = 0;
 
-    $api_login = GETPOST('QONTOSYNC_API_LOGIN', 'alpha');
-    $api_key = GETPOST('QONTOSYNC_API_KEY', 'alpha');
-    $mock_mode = GETPOST('QONTOSYNC_MOCK_MODE', 'int');
+	foreach ($constantes as $const) {
+		$val = GETPOST($const, 'alpha');
+		if (!dolibarr_set_const($db, $const, $val, 'chaine', 0, '', $conf->entity)) {
+			$error++;
+		}
+	}
 
-    if (!dolibarr_set_const($db, "QONTOSYNC_API_LOGIN", $api_login, 'CHAMP_ET_VALEUR', 0, '', $conf->entity)) $error++;
-    if (!dolibarr_set_const($db, "QONTOSYNC_API_KEY", $api_key, 'CHAMP_ET_VALEUR', 0, '', $conf->entity)) $error++;
-    if (!dolibarr_set_const($db, "QONTOSYNC_MOCK_MODE", $mock_mode, 'CHAMP_ET_VALEUR', 0, '', $conf->entity)) $error++;
-
-    if (!$error) {
-        setEventMessages($langs->trans("SetupSaved"), null, 'mesgs');
-    } else {
-        setEventMessages($langs->trans("Error"), null, 'errors');
-    }
+	if (!$error) {
+		setEventMessages($langs->trans("SetupSaved"), null, 'mesgs');
+	} else {
+		setEventMessages($langs->trans("Error"), null, 'errors');
+	}
 }
 
 /*
- * View
+ * Affichage
  */
 
-$form = new Form($db);
+$page_name = $langs->trans("QontoSyncSetup");
+llxHeader('', $page_name);
 
-llxHeader('', $langs->trans("QontoSyncSetup"));
+// Génération des onglets de l'administration
+$linkback = '<a href="'.DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1">'.$langs->trans("BackToModuleList").'</a>';
+print load_fiche_titre($page_name, $linkback, 'title_setup');
 
-$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1">' . $langs->trans("BackToModuleList") . '</a>';
-print load_fiche_titre($langs->trans("QontoSyncSetup"), $linkback, 'title_setup');
+// Barre d'onglets (vide ici car un seul onglet, mais prêt pour extension)
+$head = array();
+$h = 0;
+$head[$h][0] = dol_buildpath("/qontosync/admin/setup.php", 1);
+$head[$h][1] = $langs->trans("Settings");
+$head[$h][2] = 'settings';
+dol_fiche_head($head, 'settings', '', -1, "");
 
-print '<form method="post" action="' . $_SERVER["PHP_SELF"] . '">';
-print '<input type="hidden" name="token" value="' . newToken() . '">';
+print '<form method="post" action="'.$_SERVER["PHP_SELF"].'">';
+print '<input type="hidden" name="token" value="'.newToken().'">';
 print '<input type="hidden" name="action" value="update">';
 
 print '<table class="noborder centpercent">';
 print '<tr class="liste_titre">';
-print '<td>' . $langs->trans("Parameter") . '</td>';
-print '<td>' . $langs->trans("Value") . '</td>';
+print '<td>'.$langs->trans("Parameter").'</td>';
+print '<td>'.$langs->trans("Value").'</td>';
 print '</tr>';
 
-// API Login
+// Champ API KEY
 print '<tr class="oddeven">';
-print '<td>' . $langs->trans("QontoSyncApiLogin") . '</td>';
-print '<td><input type="text" size="40" name="QONTOSYNC_API_LOGIN" value="' . dol_escape_htmltag($conf->global->QONTOSYNC_API_LOGIN) . '"></td>';
+print '<td>'.$langs->trans("QontoSyncApiKey").'</td>';
+print '<td><input type="password" size="40" name="QONTOSYNC_API_KEY" value="'.$conf->global->QONTOSYNC_API_KEY.'"></td>';
 print '</tr>';
 
-// API Key
+// Champ SLUG
 print '<tr class="oddeven">';
-print '<td>' . $langs->trans("QontoSyncApiKey") . '</td>';
-print '<td><input type="password" size="40" name="QONTOSYNC_API_KEY" value="' . dol_escape_htmltag($conf->global->QONTOSYNC_API_KEY) . '"></td>';
-print '</tr>';
-
-// Mock Mode
-print '<tr class="oddeven">';
-print '<td>' . $form->textwithpicto($langs->trans("QontoSyncMockMode"), $langs->trans("QontoSyncMockModeHelp")) . '</td>';
-print '<td>' . $form->selectyesno("QONTOSYNC_MOCK_MODE", $conf->global->QONTOSYNC_MOCK_MODE, 1) . '</td>';
+print '<td>'.$langs->trans("QontoSyncSlug").'</td>';
+print '<td><input type="text" size="40" name="QONTOSYNC_SLUG" value="'.$conf->global->QONTOSYNC_SLUG.'"></td>';
 print '</tr>';
 
 print '</table>';
 
-print '<div class="center">';
-print '<input type="submit" class="button" value="' . $langs->trans("Save") . '">';
+print '<div class="tabsAction">';
+print '<input type="submit" class="button" value="'.$langs->trans("Save").'">';
 print '</div>';
 
 print '</form>';
+
+dol_fiche_end();
 
 llxFooter();
 $db->close();
